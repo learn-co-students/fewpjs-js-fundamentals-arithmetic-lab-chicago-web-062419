2c906ad3e0b3c113d94173337cc33a57b711d28b
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-08-10 15:29:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// const multiply = (num1, num2) => {
+//     return num1 * num2
+// }
+const num1 = 2
+const num2 = 31
+const num3 = 5
+const multiply = num1 * num2
+const random = Math.random()
+const mod = 20 % 8
+const max = Math.max(1,2,3,5,20)
+
+// console.log(multiply(2,4))
+// console.log(random())
+// console.log(mod)
+// console.log(max(1,2,3,4,100))
