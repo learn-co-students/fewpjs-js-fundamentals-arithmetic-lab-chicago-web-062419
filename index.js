f752445cfce8a75b4a0c938fb102be621d8b9f84
github username <github email address>
2019-08-12 10:02:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import { maxHeaderSize } from "http";
 
-// Write your code here
+const num1 = 2;
+const num2 = 31
 
-let num1 = 2
-let num2 = 31
-
-let multiply = num1 * num2
+const multiply = num1 * num2
 
 let random = Math.ceil(Math.random() * 1000)
 
